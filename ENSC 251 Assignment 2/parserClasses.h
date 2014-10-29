2cacdd04a69dfd72b6779c8c9590c7351799bc79
Tristan May <tristanm@sfu.ca>
2014-10-29 11:22:38 -0700

    Added Comments, function movement
    Added interface in header, moved friend functions for removing comments from the tokenizer to the token list

--- ENSC 251 Assignment 2/parserClasses.h
+++ ENSC 251 Assignment 2/parserClasses.h
@@ -1,23 +1,45 @@
-#ifndef PARSERCLASSES_H_
+#ifndef PARSERCLASSES_H_//Checks to see if this file has already been declared
 #define PARSERCLASSES_H_
+
+/*Interface:
+This .h file is a base parser class that performs a lexical analysis on an input cpp file and splits them into
+separate tokens
+Authors: Tristan May(301224317), Simon Zhong
+Changelog:
+	October 25, 2014:
+		Token class:
+			Added accessor functions
+			Added mutator functions
+			Added delete function
+		TokenList
+			Added token delete function
+			Added token append function & overloaded append function
+	October 27, 2014:
+		Added SetString function in Tokenizer
+	October 28, 2014:
+		Added base cases for preparenexttoken
+		Added special cases for double operators(exl. ->)
+		Added special case for // comments
+		Added special cases for the \n, \t and char(32)
+	October 29, 2014:
+		Debugging of #include, #ifndef, #def*/
 
 //Use only the string library DO NOT add any other libraries
 #include <string>
 
 using namespace std;
 
-//Declare your variables for storing delimiters here:
-
+//Common character array for delimiters (excluding whitespace, #)
 const char DELIM[17] = { ':', '+', '-', '(', ')', '[', ']', '.', '-', '!', '~', '\"', '*', '&', '{', '}', ';'};
 
-//Token class for a doubly-linked list of string tokens
+//Token class for a doubly-linked list of string tokens (node class)
 class Token {
 private:
 	Token *next; //Next pointer for doubly linked list
 	Token *prev; //Previous pointer for doubly linked list
 	string stringRep; //Token value
 
-	//Allow TokenList class to access Token member variables marked private
+	//Allow TokenList class to access Token member variables in private member space
 	friend class TokenList;
 
 public:
@@ -51,6 +73,10 @@ class TokenList {
 private:
 	Token *head; //Points to the head of the token list (doubly linked)
 	Token *tail; //Points to the tail of the function list (doubly linked)
+	
+	friend int removeInlineComments(TokenList &tokenList);//Friend functions to remove comment nodes (inline)
+
+	friend int removeBlockComments(TokenList &tokenList);//Friend functions to remove comment nodes (block comments)
 
 public:
 	//Default Constructor, Empty list with pointers initialized to NULL
@@ -64,16 +90,19 @@ public:
 	Token* getLast() const
 	{return(tail);}
 
-	/*Creates a new token for the string input, str
+	//Precondition: String is a non-Null value
+	/*PostCondition: Creates a new token for the string input, str
 		Appends this new token to the TokenList
 		On return from the function, it will be the last token in the list*/
 	void append(const string &str);
 
-	/*Appends the token to the TokenList if not null
+	//Precondition: Token pointer is not NULL
+	/*PostCondition: Appends the token to the TokenList if not null
 		On return from the function, it will be the last token in the list*/
 	void append(Token *token);
 
-    /*Removes the token from the linked list if it is not null
+	//Precondition: Token Pointer is not NULL
+	/*Removes the token from the linked list if it is not null
 		Deletes the token
 		On return from function, head, tail and the prev and next Tokens (in relation to the provided token) may be modified.*/
 	void deleteToken(Token *token);
@@ -99,10 +128,6 @@ private:
 
 	/*Include any helper functions here
 		e.g. trimming whitespace, comment processing*/
-
-	friend int removeInlineComments(TokenList &tokenList);
-
-	friend int removeBlockComments(TokenList &tokenList);
 
 	void resetComplete();
 
